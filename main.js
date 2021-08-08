24065fa85fd038f16367c8238722da4144b48488
Nolan Caine <nolancaine@gmail.com>
2021-08-07 18:10:20 -0600

    Fix errors in show/hide fxns

--- main.js
+++ main.js
@@ -13,17 +13,38 @@ var buttonChangeGame = document.querySelector(".button-change-game");
 // page sections
 var winsColumn = document.querySelector(".wins-column");
 var gameSelect = document.querySelector(".game-select");
+var selectMode = document.querySelectorAll(".select-mode");
 var classicMode = document.querySelector(".classic-mode");
 var altMode = document.querySelector(".alt-mode");
+var modes = document.querySelector(".modes");
 var fighters = document.querySelector(".fighters");
 
 // EVENT LISTENERS
+buttonChangeGame.addEventListener("click", showModeSelect);
+classicMode.addEventListener("click", showCharacterSelect);
+
+// FUNCTIONS
+function showModeSelect() {
+  show(modes);
+  hide(fighters);
+  hide(buttonChangeGame);
+}
+
+function showCharacterSelect() {
+  hide(modes);
+  show(fighters);
+  show(buttonChangeGame);
+}
 
 // UTILITY FUNCTIONS
 function show(element) {
-  element.classList.add(hidden);
+  element.classList.remove("hidden");
 }
 
 function hide(element) {
-  element.classList.remove(hidden);
+  element.classList.add("hidden");
+}
+
+function logTarget() {
+  console.log(event.target);
 }
