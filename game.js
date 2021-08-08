04135b243df0ad3a83ecb4bbd32f28e80f949321
Nolan Caine <nolancaine@gmail.com>
2021-08-08 15:22:48 -0600

    Write conditionals for classic mode

--- game.js
+++ game.js
@@ -11,3 +11,40 @@ class Game {
   // A way to check the Game’s board data for win conditions
   // A way to detect when a game is a draw (no one has won)
   // A way to reset the Game’s board to begin a new game
+
+// CLASSIC MODE
+
+if (player1.fighter === player2.fighter) {
+  // DRAW
+}
+
+// Rock beats Paper
+if (player1.fighter === "rock-character") {
+  if (player2.fighter === "paper-character") {
+    player2.win();
+    player2.saveWinsToStorage();
+  } else if (player2.fighter === "scissors-character") {
+    player1.win();
+    player1.saveWinsToStorage();
+  }
+} else if (player1.fighter === "paper-character") {
+  if (player2.fighter === "scissors-character") {
+    player2.win();
+    player2.saveWinsToStorage();
+  } else if (player2.fighter === "rock-character") {
+    player1.win();
+    player1.saveWinsToStorage();
+  }
+} else if (player1.fighter === "scissors-character") {
+  if (player2.fighter === "rock-character") {
+    player2.win();
+    player2.saveWinsToStorage();
+  } else if (player2.fighter === "paper-character") {
+    player1.win();
+    player1.saveWinsToStorage();
+  }
+}
+// Paper beats Scissors
+// Scissors beat Rock
+
+// ALT MODE
