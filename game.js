374936272a904fbe2302bce9b421e21d3cc1acee
Nolan Caine <nolancaine@gmail.com>
2021-08-07 23:01:53 -0600

    Add requirements list from project spec

---
+++ game.js
@@ -0,0 +1,13 @@
+class Game {
+  constructor(player1, player2) {
+
+  }
+}
+
+// A Game should include:
+  // Two Player instances
+  // A way to keep track of the data for the game board
+  // A way to keep track of the selected game type
+  // A way to check the Game’s board data for win conditions
+  // A way to detect when a game is a draw (no one has won)
+  // A way to reset the Game’s board to begin a new game
