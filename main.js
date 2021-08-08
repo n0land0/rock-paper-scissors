4952cbea024156a5e63918872d904b2118ed163a
Nolan Caine <nolancaine@gmail.com>
2021-08-07 18:19:44 -0600

    Add mode/character view switching

--- main.js
+++ main.js
@@ -13,21 +13,43 @@ var buttonChangeGame = document.querySelector(".button-change-game");
 // page sections
 var winsColumn = document.querySelector(".wins-column");
 var gameSelect = document.querySelector(".game-select");
+
+var modes = document.querySelector(".modes");
 var selectMode = document.querySelectorAll(".select-mode");
 var classicMode = document.querySelector(".classic-mode");
 var altMode = document.querySelector(".alt-mode");
-var modes = document.querySelector(".modes");
+
 var fighters = document.querySelector(".fighters");
+var fightersClassic = document.querySelector(".fighters-classic");
+var fightersAlt = document.querySelector(".fighters-alt");
 
 // EVENT LISTENERS
 buttonChangeGame.addEventListener("click", showModeSelect);
-classicMode.addEventListener("click", showCharacterSelect);
+classicMode.addEventListener("click", showClassicCharacters);
+altMode.addEventListener("click", showAltCharacters);
 
 // FUNCTIONS
+  // view switching
 function showModeSelect() {
   show(modes);
   hide(fighters);
   hide(buttonChangeGame);
+}
+
+function showClassicCharacters() {
+  show(fightersClassic);
+  if (!fightersAlt.classList.contains("hidden")) {
+    hide(fightersAlt);
+  }
+  showCharacterSelect();
+}
+
+function showAltCharacters() {
+  show(fightersAlt);
+  if (!fightersClassic.classList.contains("hidden")) {
+    hide(fightersClassic);
+  }
+  showCharacterSelect();
 }
 
 function showCharacterSelect() {
