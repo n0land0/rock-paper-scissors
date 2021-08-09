1364e3c85da00f1c5f6605efbf070ccc4a290e43
Nolan Caine <nolancaine@gmail.com>
2021-08-09 09:53:36 -0600

    Create data file for character images

---
+++ data.js
@@ -0,0 +1,12 @@
+var classicCharacters = {
+  rock: "assets/Rock.svg",
+  paper: "assets/Paper.svg",
+  scissors: "assets/Scissors.svg"
+};
+var altCharacters = {
+  spongebob: "assets/SpongeBob_SquarePants_character.svg.png",
+  patrick: "assets/Patrick.png",
+  squidward: "assets/Squidward_Tentacles.svg.png",
+  sandy: "assets/Sandy_Cheeks.svg.png",
+  krabs: "assets/Mr._Krabs.svg.png"
+};
