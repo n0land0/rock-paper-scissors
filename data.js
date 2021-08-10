05983969ca8f32f35e48f6db0ffd0654ef339a6d
Nolan Caine <nolancaine@gmail.com>
2021-08-10 16:34:56 -0600

    Sketch alternate game.runGame method

--- data.js
+++ data.js
@@ -21,9 +21,27 @@ var altCharacters = [
 // REFACTOR GOALS
 // write game.runGame to be one conditional
   // requires gameType values to match names of condition properties
-// deal with alt tags on empty img elements
 
 // ✅ replace setInterval w/ setTimeout & eliminate global variable
 // ✅ remove placeholder from HTML current fighters
 // ✅ convert fighter imgs to buttons
 // ✅ which classes, query selectors am I not using?
+// ✅ deal with alt tags on empty img elements
+
+
+
+// runGame() {
+//   if (this.player1.fighter === this.player2.fighter) {
+//     this.winner = null;
+//     return "Draw!";
+//   } else {
+//     if (this[this.gameType][this.player1.fighter].includes(this.player2.fighter)) {
+//       this.winner = this.player1.name;
+//       this.player1.win();
+//     } else if (this[this.gameType][this.player2.fighter].includes(this.player1.fighter)) {
+//       this.winner = this.player2.name;
+//       this.player2.win();
+//     }
+//     return `${this.winner} wins!`;
+//   }
+// }
