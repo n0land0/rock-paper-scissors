28693e7bd4328edbf21749dc33c0c4f2fb6edf9e
Nolan Caine <nolancaine@gmail.com>
2021-08-09 18:52:16 -0600

    Clean up old comments/pseudo

--- player.js
+++ player.js
@@ -14,10 +14,6 @@ class Player {
     localStorage.setItem(this.name, this.wins);
   }
   retrieveWinsFromStorage() {
-    // var fromStorage = localStorage.getItem(this.name);
-    // var parsed = JSON.parse(fromStorage);
-    // return parsed;
-
     if (localStorage[this.name]) {
       this.wins = localStorage.getItem(this.name);
     }
@@ -32,23 +28,5 @@ class Player {
         this.fighter = altCharacters[randomIndex];
       }
     }
-    // assign clicked character to this.fighter
   }
 }
-
-// PSEUDOCODE
-// Player 1 is Player instance named "Human"
-// Player 2 is Player instance named "Computer"
-  // this is defaulted to on page load
-// Game consists of each player choosing a character - player 1, then player 2
-  // choices must be registered in data
-// Winner is determined by conditionals which compare characters
-  // "rock beats scissors, etc.
-  // if p1 > rock and p2 > scissors,
-  // then p1 adds a win;
-  // if p1 > rock & p2 > rock,
-  // neither player adds a win -
-  // register a draw"
-// win is immediately saved to storage;
-// wins in storage need to be overwritten;
-// wins retrieved from storage for DOM wins column
